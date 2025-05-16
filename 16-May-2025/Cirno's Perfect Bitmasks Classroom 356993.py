d315fda90b03729df1810938da4a23f9c26b65ab
Abdrehim Misbah <abdellaabdre@gmail.com>
2025-05-16 17:42:54 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 16-May-2025/Cirno's Perfect Bitmasks Classroom 356993.py
@@ -0,0 +1,28 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+nTest = int(input())
+for _ in range(nTest):
+    x = int(input())
+    if x & 1 == 1:
+        x >>= 1
+        while x > 0 and x & 1 != 1:
+            x >>= 1
+        if x > 0:
+            print(1)
+        else:
+            print(3)
+    else:
+        i = 1
+        x >>= 1
+        while x > 0 and x & 1 != 1:
+            x >>= 1
+            i += 1
+        j = i
+        x >>= 1
+        while x > 0 and x & 1 != 1:
+            x >>= 1
+            i += 1
+        if x > 0:
+            print((1<<j))
+        else:
+            print((1<<j) + 1)
