511cfb1ffd0ad432f9a9d22e8c3d10abcfb8f9bf
Abdrehim Misbah <abdellaabdre@gmail.com>
2025-08-02 17:55:04 +0300

    Created 'N-Queens' by A2SV Hub

---
+++ 02-Aug-2025/N-Queens 381357.py
@@ -0,0 +1,28 @@
+# Problem: N-Queens - https://leetcode.com/problems/n-queens/description/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        self.answer = []
+
+        def backtrack(row, queensPosition):
+            if row == n:
+               
+                board = []
+                for _, col in sorted(queensPosition):
+                    row_str = ['.'] * n
+                    row_str[col] = 'Q'
+                    board.append(''.join(row_str))
+                self.answer.append(board)
+                return
+
+            for col in range(n):
+                for r, c in queensPosition:
+                    if c == col or abs(r - row) == abs(c - col):
+                        break
+                else:
+                    queensPosition.append((row, col))
+                    backtrack(row + 1, queensPosition)
+                    queensPosition.pop()
+
+        backtrack(0, [])
+        return self.answer
