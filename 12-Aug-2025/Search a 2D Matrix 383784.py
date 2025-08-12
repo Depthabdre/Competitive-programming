6ce5ffc925d2b9713d2b45ab9054124a7aff8327
Abdrehim Misbah <abdellaabdre@gmail.com>
2025-08-12 10:30:38 +0300

    Created 'Search a 2D Matrix' by A2SV Hub

---
+++ 12-Aug-2025/Search a 2D Matrix 383784.py
@@ -0,0 +1,20 @@
+# Problem: Search a 2D Matrix - https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        left = 0
+        right = len(matrix) * len(matrix[0]) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // len(matrix[0])
+            col = mid % len(matrix[0])
+
+            if matrix[row][col] > target:
+                right = mid - 1
+            elif matrix[row][col] < target:
+                left = mid + 1
+            else:
+                return True
+        return False
