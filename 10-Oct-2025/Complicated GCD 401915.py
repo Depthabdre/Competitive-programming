9b30b22b8bbec1cca365a6f2504a702b31bda0e8
Abdrehim Misbah <abdellaabdre@gmail.com>
2025-10-10 18:36:10 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 10-Oct-2025/Complicated GCD 401915.py
@@ -0,0 +1,34 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+# Abdre
+from math import ceil, sqrt, log, log2, pow, floor, gcd, inf, isqrt, lcm
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+rand = randint(1, 10000)
+xor = lambda x: x ^ rand
+prefix_sum = lambda arr: list(itertools.accumulate(arr))
+
+
+
+def solve():
+    numa , numb = words()
+    if numa == numb:
+        print(numa)
+    else:
+        print(1)
+    
+    return
+
+
+for _ in range(test_cases(1)):
+    solve()
