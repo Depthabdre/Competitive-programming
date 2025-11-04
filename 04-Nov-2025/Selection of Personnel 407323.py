0bf0e9adb2e6a14497fc953c0dc21a2a339bb18a
Abdrehim Misbah <abdellaabdre@gmail.com>
2025-11-04 17:17:25 +0300

    Created 'Selection of Personnel' by A2SV Hub

---
+++ 04-Nov-2025/Selection of Personnel 407323.py
@@ -0,0 +1,31 @@
+# Problem: Selection of Personnel - https://codeforces.com/problemset/problem/630/F
+
+# Abdre
+from math import ceil, sqrt, log, log2, pow, floor, gcd, inf, isqrt, lcm
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+rand = randint(1, 10000)
+xor = lambda x: x ^ rand
+prefix_sum = lambda arr: list(itertools.accumulate(arr))
+
+
+
+def solve():
+    n = number()
+    print(math.comb(n, 5) + math.comb(n, 6) + math.comb(n, 7))
+    
+    return
+
+
+for _ in range(test_cases(1)):
+    solve()
